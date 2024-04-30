5ddeeea3f59a345492514c2e2af41914dc70bf1c
Teze14 <143450321+Teze14@users.noreply.github.com>
2024-04-30 17:05:54 -0600

    h

--- qr_navigation_2/pyzedar.py
+++ qr_navigation_2/pyzedar.py
@@ -1,75 +1,71 @@
-import cv2
-import numpy as np
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from std_msgs.msg import Int8
+import time
 import math
-import pyzed.sl as sl  # Importar la librería PyZED
+import cv2
 
-parametros = cv2.aruco.DetectorParameters()
-diccionario = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
+class Camera(Node):
+	def __init__(self):
+		super().__init__("Camera_Node")
+		self.arm_cam_publisher = self.create_publisher(Image, "/arm_cam", 10)
+		self.ant_cam_publisher = self.create_publisher(Image, "/ant_cam", 10)
+		self.create_subscription(Int8, "image_quality", self.quality_callback, 1)
+		self.quality = 18
+		self.bridge = CvBridge()
+		self.timer = self.create_timer(0.0001, self.cameras)
 
-# Configurar la cámara ZED
-init_params = sl.InitParameters()
-init_params.camera_resolution = sl.RESOLUTION.HD720
-init_params.camera_fps = 30
-zed = sl.Camera()
-err = zed.open(init_params)
-if err != sl.ERROR_CODE.SUCCESS:
-    print("Error al abrir la cámara ZED")
-    exit(-1)
+		# Inicializar cámaras
+		self.arm_camera = self.initialize_camera()
+		self.antenna_camera = self.initialize_camera()
+  
+	def quality_callback(self, msg):
+		self.quality = msg.data
+  
+	def initialize_camera(self):
+		num_cameras = 5  # Rango de posibles indices generados
+		for index in range(num_cameras):
+			camera = cv2.VideoCapture(index)
+			if camera.isOpened():
+				self.get_logger().info(f"Cámara encontrada en el índice {index}")
+				return camera
+			camera.release()
 
-# Poner como constante el tamaño físico del marcador ArUco en centímetros
-marcador_cm = 20.0
+		self.get_logger().error("No se encontraron cámaras disponibles.")
+		return None
 
-while True:
-    # Capturar fotograma de la cámara ZED
-    if zed.grab() == sl.ERROR_CODE.SUCCESS:
-        # Obtener la imagen izquierda (puedes usar la derecha si prefieres)
-        image_zed = sl.Mat()
-        zed.retrieve_image(image_zed, sl.VIEW.LEFT)
-        frame = image_zed.get_data()
+	def cv2_to_imgmsg(self, image):
+		msg = self.bridge.cv2_to_imgmsg(image, encoding = "bgr8")
+		return msg
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	def cv2_to_imgmsg_resized(self, image, scale_percent):
+		width = int(image.shape[1] * scale_percent / 100)
+		height = int(image.shape[0] * scale_percent / 100)
+		dim = (width, height)
+		resized_image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+		msg = self.bridge.cv2_to_imgmsg(resized_image, encoding = "bgr8")
+		return msg
 
-        esquinas, ids, _ = cv2.aruco.detectMarkers(gray, diccionario, parameters=parametros)
+	def cameras(self):
+		if self.arm_camera:
+			ret_arm, frame_arm = self.arm_camera.read()
+			if ret_arm:
+				self.arm_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_arm, self.quality))
 
-        if np.all(ids is not None):
-            for i, esquina in enumerate(esquinas):
-                id_marcador = ids[i][0]
-                x, y = map(int, esquina[0][0])
-                cv2.putText(frame, str(id_marcador), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		if self.antenna_camera:
+			ret_ant, frame_ant = self.antenna_camera.read()
+			if ret_ant:
+				self.ant_cam_publisher.publish(self.cv2_to_imgmsg_resized(frame_ant, self.quality))
 
-                marcador_pixeles = max(esquina[0][0]) - min(esquina[0][0])
-                distancia_aprox_cm = (marcador_cm * 100) / marcador_pixeles
+def main(args=None):
+	rclpy.init(args=args)
+	cameras = Camera()
+	rclpy.spin(cameras)
+	cameras.zed.close()
+	cameras.destroy_node()
+	rclpy.shutdown()
 
-                centro_x = int((esquina[0][0][0] + esquina[0][2][0]) / 2)
-                centro_y = int((esquina[0][0][1] + esquina[0][2][1]) / 2)
-
-                punto_izquierda = (int(esquina[0][0][0]), centro_y)
-                punto_derecha = (int(esquina[0][2][0]), centro_y)
-
-                distancia_centro_izquierda = math.sqrt((centro_x - punto_izquierda[0]) ** 2 + (centro_y - punto_izquierda[1]) ** 2)
-                distancia_centro_derecha = math.sqrt((centro_x - punto_derecha[0]) ** 2 + (centro_y - punto_derecha[1]) ** 2)
-
-                cv2.putText(frame, f'Distancia: {distancia_aprox_cm:.2f} cm', (x, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.circle(frame, (centro_x, centro_y), 4, (0, 0, 255), -1)
-                cv2.circle(frame, punto_izquierda, 4, (255, 0, 0), -1)
-                cv2.circle(frame, punto_derecha, 4, (255, 0, 0), -1)
-
-                if distancia_centro_izquierda < distancia_centro_derecha:
-                    cv2.putText(frame, f'Ve a la izquierda', (150, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                elif distancia_centro_derecha < distancia_centro_izquierda:
-                    cv2.putText(frame, f'Ve a la derecha', (150, 250), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                else:
-                    cv2.putText(frame, f'Estás centrado', (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-                cv2.putText(frame, f'Distancia Centro a Izquierda: {distancia_centro_izquierda:.2f} px', (x, y + 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.putText(frame, f'Distancia Centro a Derecha: {distancia_centro_derecha:.2f} px', (x, y + 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        cv2.imshow("Frame", frame)
-
-    # Terminar el programa con la tecla 'esc'
-    k = cv2.waitKey(1)
-    if k == 27:
-        break
-
-zed.close()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+	main()
